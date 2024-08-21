1998266d47415b59dfc07ceec11d8e89af822f86
JP <jp.sherka@gmail.com>
2024-08-21 17:05:37 -0400

    add table

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ToggleColorMode from './components/ToggleColorMode';
 import { SnackbarContextProvider } from './context/SnackbarContext';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import TableStreet from './components/TableStreet';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
           <Box sx={{ textAlign: 'center' }}>
             <ToggleColorMode/>
             <Typography variant="h3" gutterBottom> Tech Interview Holdconet</Typography>
+            <TableStreet/>
           </Box>
         </>
       </SnackbarContextProvider>
